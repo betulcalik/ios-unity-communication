a64f60df8f87f22b103c88dec15aaca1be942e54
Betül Çalık <betulcalik82@icloud.com>
2023-08-26 20:24:39 +0300

    added buttons for opening to unity

---
+++ ios-unity-communication/Managers/UnityManager.swift
@@ -0,0 +1,144 @@
+//
+//  UnityManager.swift
+//  ios-unity-communication
+//
+//  Created by Betül Çalık on 26.08.2023.
+//
+
+import Foundation
+import UnityFramework
+
+final class UnityManager: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - Variables
+    static let shared = UnityManager()
+    
+    private let dataBundleId = "com.unity3d.framework"
+    private let frameworkPath = "/Frameworks/UnityFramework.framework"
+    
+    private var unityFramework: UnityFramework?
+    private var hostMainWindow: UIWindow?
+    private var launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    
+    private var isInitialized: Bool {
+        return unityFramework != nil && unityFramework?.appController() != nil
+    }
+    
+    // The structure for Unity messages
+    private struct UnityMessage {
+        let objectName: String?
+        let methodName: String?
+        let messageBody: String?
+    }
+    private var cachedMessages = [UnityMessage]()
+    
+    // MARK: - Public Functions
+    func show() {
+        if isInitialized {
+            showUnityWindow()
+            return
+        }
+        
+        initUnityWindow()
+    }
+    
+    func setHostMainWindow(_ hostMainWindow: UIWindow?) {
+        self.hostMainWindow = hostMainWindow
+    }
+    
+    /// Send Message to Unity
+    func sendMessage(_ objectName: String, methodName: String, message: String) {
+        let message: UnityMessage = UnityMessage(
+            objectName: objectName,
+            methodName: methodName,
+            messageBody: message
+        )
+        
+        if isInitialized {
+            unityFramework?.sendMessageToGO(
+                withName: message.objectName,
+                functionName: message.methodName,
+                message: message.messageBody
+            )
+        } else {
+            cachedMessages.append(message)
+        }
+    }
+    
+    // MARK: - Private Functions
+    private func initUnityWindow() {
+        guard let unityFramework = loadUnityFramework() else {
+            debugPrint("ERROR: Was not able to load Unity")
+            return unloadUnityWindow()
+        }
+        
+        self.unityFramework = unityFramework
+        self.unityFramework?.setDataBundleId(dataBundleId)
+        self.unityFramework?.register(self)
+        self.unityFramework?.runEmbedded(withArgc: CommandLine.argc,
+                                         argv: CommandLine.unsafeArgv,
+                                         appLaunchOpts: launchOptions)
+        
+        sendCachedMessages()
+    }
+    
+    private func showUnityWindow() {
+        if isInitialized {
+            unityFramework?.showUnityWindow()
+            sendCachedMessages()
+        }
+    }
+    
+    private func unloadUnityWindow() {
+        if isInitialized {
+            cachedMessages.removeAll()
+            unityFramework?.unloadApplication()
+        }
+    }
+    
+    private func loadUnityFramework() -> UnityFramework? {
+        let bundlePath: String = Bundle.main.bundlePath + frameworkPath
+        let bundle = Bundle(path: bundlePath)
+        
+        if bundle?.isLoaded == false {
+            bundle?.load()
+        }
+        
+        let unityFramework = bundle?.principalClass?.getInstance()
+        if unityFramework?.appController() == nil {
+            let machineHeader = UnsafeMutablePointer<MachHeader>.allocate(capacity: 1)
+            machineHeader.pointee = _mh_execute_header
+            
+            unityFramework?.setExecuteHeader(machineHeader)
+        }
+        
+        return unityFramework
+    }
+    
+    private func sendCachedMessages() {
+        if cachedMessages.count >= 0 && isInitialized {
+            for message in cachedMessages {
+                unityFramework?.sendMessageToGO(
+                    withName: message.objectName,
+                    functionName: message.methodName,
+                    message: message.messageBody
+                )
+            }
+            
+            cachedMessages.removeAll()
+        }
+    }
+    
+}
+
+// MARK: - Unity Framework Listener
+extension UnityManager: UnityFrameworkListener {
+    
+    func unityDidUnload(_ notification: Notification!) {
+        unityFramework?.appController().rootViewController.dismiss(animated: true)
+        unityFramework?.unregisterFrameworkListener(self)
+        unityFramework = nil
+        hostMainWindow?.makeKeyAndVisible()
+    }
+    
+}
--- ios-unity-communication/ViewController.swift
+++ ios-unity-communication/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI Components
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    
+    // MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+    }
+    
+    private func setupUI() {
+        redButton.layer.cornerRadius = redButton.frame.height / 2
+        blueButton.layer.cornerRadius = blueButton.frame.height / 2
+        greenButton.layer.cornerRadius = greenButton.frame.height / 2
     }
 
-
+    // MARK: - Actions
+    @IBAction func didRedButtonTap(_ sender: UIButton) {
+        UnityManager.shared.show()
+        UnityManager.shared.sendMessage("Ball",
+                                        methodName: "SetBallColor",
+                                        message: "red")
+    }
+    
+    @IBAction func didBlueButtonTap(_ sender: UIButton) {
+        UnityManager.shared.show()
+        UnityManager.shared.sendMessage("Ball",
+                                        methodName: "SetBallColor",
+                                        message: "blue")
+    }
+    
+    @IBAction func didGreenButtonTap(_ sender: UIButton) {
+        UnityManager.shared.show()
+        UnityManager.shared.sendMessage("Ball",
+                                        methodName: "SetBallColor",
+                                        message: "green")
+    }
+    
 }
 
